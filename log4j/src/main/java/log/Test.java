c19dbb28df3fa7a5690f112796ee07912b5b85c8
feizai1012 <1429758925@qq.com>
2023-02-21 15:58:36 +0800

    2222

--- log4j/src/main/java/log/Test.java
+++ log4j/src/main/java/log/Test.java
@@ -8,7 +8,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        log.error("我是error,error,error44444444444444");
+        log.error("我是error,error,error5555555555");
         log.info("我是info,info,info");
         log.debug("我是debug,debug,debug");
         
